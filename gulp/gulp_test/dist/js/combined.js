957256a894f8694ad6e5ca1b424c2dd7ebbc2c58
bsia0831 <ye08312@naver.com>
2021-11-24 16:28:54 +0900

    gulp

---
+++ gulp/gulp_test/dist/js/combined.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    var num = 0;
+    console.log('visual');
+})
+$(document).ready(function(){
+    var num = 1;
+    $('#gnb>li').on('mouseenter',function(){
+        $(this).children('ul').show();
+        $(this).children('a').addClass('on');
+    });
+    $('#gnb>li').on('mouseleave',function(){
+        $(this).children('ul').hide();
+        $(this).children('a').removeClass('on');
+    });
+})
+$(document).ready(function(){
+    var num = 1;
+    $('#gnb>li').on('mouseenter',function(){
+        $(this).children('ul').show();
+        $(this).children('a').addClass('on');
+    });
+    $('#gnb>li').on('mouseleave',function(){
+        $(this).children('ul').hide();
+        $(this).children('a').removeClass('on');
+    });
+})
