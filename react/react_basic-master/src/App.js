767a9cdcce95bfd6aeff3a4d7edf7862333c892a
bsia0831 <ye08312@naver.com>
2021-12-14 23:00:57 +0900

    react

---
+++ react/react_basic-master/src/App.js
@@ -0,0 +1,16 @@
+import './css/style.css';
+import {Logo, Menu} from './components/Layout';
+import Footer from './components/Footer';
+
+function App() {
+	return (
+		<figure>
+			<Logo />
+			<Menu />			
+			
+			<Footer />
+		</figure>
+	);
+}
+
+export default App;
